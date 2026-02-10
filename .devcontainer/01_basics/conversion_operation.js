de82ee95ca09021483af3b4660112bc6d7929dcf
anandiit2025-ui <anandiit2025@gmail.com>
2026-02-10 18:05:21 +0000

    conversion operation

---
+++ .devcontainer/01_basics/conversion_operation.js
@@ -0,0 +1,26 @@
+let score="hitesh"
+
+ console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score);
+
+console.log(valueInNumber)
+ console.log(valueInNumber )
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true =>1 , false => 0
+
+let isLoggedIn ="hitesh";
+let booleanisloggedIn = Boolean(isLoggedIn)
+console.log(booleanisloggedIn)
+
+// 1 =>true; 0=>false
+//"" =>false
+//"hitesh" =>true
+
+let someNumber = 33;
+let stringnumber =String(someNumber)
+
+console.log(typeof stringnumber);
