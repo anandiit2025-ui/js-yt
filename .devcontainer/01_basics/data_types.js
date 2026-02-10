a16ece62d0bd2c84c8e7c3b5dfd05a4b3c7af5e7
anandiit2025-ui <anandiit2025@gmail.com>
2026-02-10 17:47:21 +0000

    data types

---
+++ .devcontainer/01_basics/data_types.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version
+ // alert(3+3) // we are using node.js not browser, so alert is not defined
+    console.log(3+
+        3) // this will work in node.js, it will print 6 to the console
+        // code readibility should be high
+
+    console.log("Hello World")
+
+    let name ="anand"
+    let age =18;
+    let istrue = false;
+
+    // number => 2 to power 53
+    //bigint => more than 2 to power 53
+    // string =>""
+    // boolean = > true or false
+    // null => empty value , standalone value
+    // undefined => value is not assigned
+
+    // symbol =>unique
+    //object
+
+    console.log(typeof null); // object
+    console.log(typeof undefined); // undefined
+
